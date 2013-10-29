b33dccbf5d19cab36108d720b205530762d6ce4b
diplomovka <michal.trunek@gmail.com>
2013-10-29 16:21:15 +0100

    commit

--- application/views/view_home.php
+++ application/views/view_home.php
@@ -3,17 +3,26 @@
 <head>
 	<meta charset="utf-8">
 	<title><?php echo $title; ?></title>
+   
+    <link rel="stylesheet" href="css/main.css" type="text/css"/>
 	
+	<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+	
+	<script src=”bootstrap/js/bootstrap.js”></script>
+
+	 
 </head>
 <body>
-<base href="<?php echo base_url();?>" />
-<div id="container">
+<base href="<?php echo base_url(); ?>" />
+<div class="container">
 	<h1>Welcome to CodeIgniter!</h1>
 
 
-	
-	<ul>
-		<li>
+<div class="navbar">	
+	<div class="navbar-inner">
+		<div class="container">
+	<ul class="nav">
+		<li class="active">
 			<a href="#">Úvod</a>
 		</li>
 		<li>
@@ -32,8 +41,89 @@
 			<a href="kontakt">Kontakt</a>
 		</li>
 	</ul>
+	</div>
+	</div>
+</div>
+
+<div class="hero-unit">
+    <h1>Marketing stuff bitches!</h1>
+ 
+    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+ 
+    <a href="#" class="btn btn-large btn-success">Get Started</a>
+ </div>
+ 
+ <div class="row">
+         <div class="span4">
+             <ul class="nav nav-list">
+        <li class="nav-header">
+			Menu
+		</li>
+		<li class="active">
+			<a href="#">Úvod</a>
+		</li>
+		<li>
+			<a href="about">Ubytovanie</a>
+		</li>
+		<li>
+			<a href="lokalita">Lokalita</a>
+		</li>
+		<li>
+			<a href="fotos">Fotogaléria</a>
+		</li>
+		<li>
+			<a href="cennik">Cenník</a>
+		</li>
+		<li>
+			<a href="kontakt">Kontakt</a>
+		</li>
+	</ul>
+         </div>
+         <div class="span8">
+             <h3>How we started ?</h3>
+ 
+<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+ 
+<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+ 
+<h3>How do we market?</h3>
+ 
+<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+ 
+<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+
+         </div>
+     </div>
+     
+     <hr></hr>
+     <div class="row">
+<div class="span4">
+        <h4 class="muted text-center">Meet Our Clients</h4>
+        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+        <a href="#" class="btn"><i class="icon-user"></i> Our Clients</a>
+    </div>
+    <div class="span4">
+        <h4 class="muted text-center">Know Our Employees</h4>
+        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+        <a href="#" class="btn btn-success"><i class="icon-star icon-white"></i> Our Employees</a>
+    </div>
+    <div class="span4">
+        <h4 class="muted text-center">Reach Us</h4>
+        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+        <a href="#" class="btn btn-info">Contact Us</a>
+    </div>
+</div>
+<hr>
+<div class="footer">
+<p>&copy; 2013</p>
+</div>
 	
 </div>
 
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.css" type="text/css"/>
+	
+	<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+	
+	 <script src=”bootstrap/js/bootstrap.js”></script>
 </body>
 </html>
